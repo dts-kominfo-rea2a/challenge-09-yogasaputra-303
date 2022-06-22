f9a11dd573f6058642b456249cd2534b7ec8385b
Yoga Saputra <yogsyogs98@gmail.com>
2022-06-22 09:39:33 +0700

    Tugas Pengenalan React

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
-    )
-}
+      <div className="header">
+        <h2>Call a Friend</h2>
+        <div>
+          <span>Your friendly contact app</span>
+        </div>
+      </div>
+    );
+};
 
 export default Header;
